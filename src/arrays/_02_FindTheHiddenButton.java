770125a9453cbb1bc628e3af16f962fe86380162
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-12-17 17:48:27 -0800

    .

--- src/arrays/_02_FindTheHiddenButton.java
+++ src/arrays/_02_FindTheHiddenButton.java
@@ -20,9 +20,9 @@ public class _02_FindTheHiddenButton implements ActionListener{
 	JPanel panel;
 	
 	//1. create an array of JButtons. Don't initialize it yet.
-	
+	JButton[] button;
 	//2 create an int variable called hiddenButton
-	
+	int hiddenButton;
 	public static void main(String[] args) {
 		new _02_FindTheHiddenButton().start();
 	}
@@ -33,8 +33,11 @@ public class _02_FindTheHiddenButton implements ActionListener{
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		//3.  Ask the user to enter a positive number and convert it to an int
-		
+		String posNum = JOptionPane.showInputDialog("Enter a positive number");
+		int posNumb = Integer.parseInt(posNum);
 		//4. Initialize the array of JButtons to be the size of the int created in step 3
+		
+			button = new JButton
 		
 		//5. Make a for loop to iterate through the JButton array
 			//6. initialize each JButton in the array
